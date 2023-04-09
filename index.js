89fffa52a068d5c4b8181e0182779905d5a3306e
Kirill Komarov <riperocks98@gmail.com>
2023-04-10 00:40:50 +0300

    upload API

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let result = document.getElementById("result");
 let getMovie = () => {
     const APIKey = 'db5ed904';
     let movieName = movieNameRef.value;
-    let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${APIKey}`;
+    let url = `https://www.omdbapi.com/?t=${movieName}&apikey=${APIKey}`;
 
     //if input field is empty
 
